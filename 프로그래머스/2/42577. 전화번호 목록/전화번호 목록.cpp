417064443e91771c278a07a65521be9ff99bdf3e
myeongsung kim <dreamkms2014@pusan.ac.kr>
2026-02-25 12:45:23 +0900

    [level 2] Title: 전화번호 목록, Time: 80.53 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    int len = phone_book.size();
+    
+    sort(phone_book.begin(), phone_book.end()); 
+    
+    for(int i = 0; i < len -1; i++){
+        if(phone_book[i+1].find(phone_book[i]) == 0){
+            return false;
+        }
+    }
+    
+    return answer;
+}
